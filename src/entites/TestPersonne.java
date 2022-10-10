bb9be44ce6352d258b9edd51363e5b36c9f6209e
Manuel <tyranidas@hotmail.com>
2022-10-10 17:30:59 +0200

    tp3

--- src/entites/TestPersonne.java
+++ src/entites/TestPersonne.java
@@ -9,9 +9,13 @@ public class TestPersonne {
 		Personne bob = new Personne("Morane", "Bob", new AdressePostale(5, "rue de la Liberté", 44200, "Nantes"));
 		
 		Personne martine = new Personne("A la plage", "Martine", new AdressePostale(5, "rue de la Liberté", 44200, "Nantes"));
-		martine.nom = "A la plage";
-		martine.prénom ="Martine";
-		martine.adresse = null;
+		
+		String bigName = martine.majName(martine.nom);
+		martine.setAdresse(15, "rue trucmuche", 44610, "Indre");
+		System.out.println(bigName);
+		System.out.println(bob.prénom);
+		AdressePostale name = martine.getAdresse();
+		System.out.println(martine.adresse.libelléDeRue);
 	}
 
 }
--- src/entites2/Personne.java
+++ src/entites2/Personne.java
@@ -9,12 +9,48 @@ public class Personne {
 		public String prénom;
 		public AdressePostale adresse;
 		
-		public Personne (String NvNom, String NvPrenom, AdressePostale NvAdresse)
+		public Personne (String nvNom, String nvPrenom, AdressePostale nvAdresse)
 		{
-			nom = NvNom;
-			prénom = NvPrenom;
-			adresse = NvAdresse;
+			this.nom = nvNom;
+			this.prénom = nvPrenom;
+			this.adresse = nvAdresse;
 		}
 	
-
+		public String majName (String nvNom)
+		{
+			String upperNom = nvNom.toUpperCase()+" "+prénom;
+			return upperNom;
+		}
+		
+		public void setName (String nvNom)
+		{
+			nom = nvNom;
+		}
+		public String getName ()
+		{
+			return nom;
+		}
+		
+		public void setFirstName (String nvPrénom)
+		{
+			prénom = nvPrénom;
+			
+		}
+		public String getFirstName ()
+		{
+			return prénom;
+		}
+		public void setAdresse (int nvNumero, String nvRue, int nvCodePostal, String nvVille)
+		{
+			adresse.numeroDeRue = nvNumero;
+			adresse.libelléDeRue = nvRue;
+			adresse.codePostal = nvCodePostal;
+			adresse.ville = nvVille;
+		}
+		public AdressePostale getAdresse ()
+		{
+			return adresse;
+		}
+		
 }
+
